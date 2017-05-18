017b8836f56c4b92ae66338430b92eeca7ba656d
Amit Patel <redblobgames@gmail.com>
2017-05-17 19:18:53 -0700

    Move styling for the "exploded layers" view to the CSS

--- explode-layers.js
+++ explode-layers.js
@@ -2,22 +2,14 @@
 // Copyright 2017 Red Blob Games <redblobgames@gmail.com>
 // License: Apache v2.0 <http://www.apache.org/licenses/LICENSE-2.0.html>
 
-// Assuming the svg has a bunch of <g> elements that are 550x220,
-// "explode" them all so that you can see the layers.
+// Assuming the svg has a bunch of <g> elements "explode" them all so
+// that you can see the layers. See the CSS for the styling+animation.
 d3.select("svg").selectAll("g")
     .each(function(_, i) {
-        let width = 550, height = 220;
         let g = d3.select(this);
         g.attr('class', `layer layer-${i}`);
         g.insert('rect', ":first-child")
-            .attr('class', "glass")
-            .attr('width', width)
-            .attr('height', height)
-            .attr('fill', "hsl(120,10%,90%)")
-            .attr('fill-opacity', 0.3)
-            .attr('stroke', "hsl(120,50%,10%)")
-            .attr('stroke-opacity', 0.7)
-            .attr('stroke-width', 2);
+            .attr('class', "glass");
     });
 d3.select("body").insert('button', ":first-child")
     .text("hide layers")
